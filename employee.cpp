0a3bedbeb485ed0490a09ceaa18fdabe02629ac6
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-18 00:22:55 +0530

    hiring process

--- employee.cpp
+++ employee.cpp
@@ -2,6 +2,43 @@
 #include<fstream>
 using namespace std;
 
+void hiringProcess()
+{
+    cout<<"\n-----Hiring Process-----\n";
+    cout<<"\nRegular hiring process? (yes/no)\n";
+    string hir;
+    cin>>hir;
+    if(hir=="yes")
+    {
+        cout<<"\nPost job manually\n";
+        cout<<"\nAny suitable candidate? (yes/no)\n";
+        string suit;
+        cin>>suit;
+        if(suit=="yes")
+        {
+            cout<<"yes";
+            // interview();
+        }
+        else
+        {
+            cout<<"no";
+            // reviewCandidate();
+        }
+    }
+    else
+    {
+        cout<<"\nSpecial Hiring Process\n";
+        // cout<<"\nDescribe the special process:\n";
+        // string spec;
+        // getline(cin,spec);
+        // fstream sp("specialProcess.txt", ios::app | ios::out);
+        // sp<<spec<<endl;
+        // sp.close();
+        cout<<"\nYou can check out the special hiring process in 'specialProcess.txt' file\n";
+
+    }
+}
+
 void description()
 {
     cout<<"\n-----Job Description-----\n";
@@ -10,6 +47,7 @@ void description()
     cout<<"\nDescribe the job description:\n";
     getline(cin,des);
     jd<<des<<endl;
+    jd.close();
     cout<<"\nYou can now check out the job description in 'jd.txt' file\n";
     cout<<"\nSend crew request to HR\n";
     cout<<"\nDetermine Classifications\n";
@@ -19,5 +57,6 @@ int main()
 {
     cout<<"-----Employee Hiring Process-----\n";
     description();
-    // hiringProcess();
+    hiringProcess();
+    cout<<"\n------End of Hiring Process-----\n";
 }
